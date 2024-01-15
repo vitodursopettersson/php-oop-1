9690dcfed467c281d3aa790c5232992bbaecf609
vitodursopettersson <vitodurso98@gmail.com>
2024-01-15 17:05:53 +0100

    static class method to transform running time

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+// Classe
 class Movie
 {
     public $name;
@@ -16,7 +18,7 @@ class Movie
         $this->info = $_info;
     }
 
-    public function getRunningTimeHourMinutes($minutes)
+    static function getRunningTimeHourMinutes($minutes)
     {
         $hour = floor($minutes / 60);
         $minutes_remaining = $minutes % 60;
@@ -25,19 +27,7 @@ class Movie
     }
 };
 
+// Istanza
 $the_game_nessuna_regola = new Movie('The Game Nessuna Regola', '1997', '128', 'La vita di un consulente finanziario precipita nel pericolo e nella paranoia dopo lo strano regalo di compleanno del fratello: la partecipazione a un gioco misterioso.');
 $the_game_nessuna_regola->$category = ['Thriller', 'Drammatico', 'Premiato'];
-
-$the_game_nessuna_regola->running_time_hour_minutes = $the_game_nessuna_regola->getRunningTimeHourMinutes(128);
-
-echo $the_game_nessuna_regola->name;
-echo "<br>";
-echo $the_game_nessuna_regola->year;
-echo "<br>";
-echo $the_game_nessuna_regola->running_time_minutes;
-echo "<br>";
-var_dump($the_game_nessuna_regola->running_time_hour_minutes);
-echo "<br>";
-var_dump($the_game_nessuna_regola->$category);
-echo "<br>";
-echo $the_game_nessuna_regola->info;
+$the_game_nessuna_regola->running_time_hour_minutes = Movie::getRunningTimeHourMinutes(128);
